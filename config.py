8df601cf22cc044f7c74d027172310c962207043
eratine667-sys <eratine667@gmail.com>
2026-02-27 16:57:45 +0300

    Update config.py

--- config.py
+++ config.py
@@ -4,6 +4,4 @@ from dotenv import load_dotenv
 load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-
-if not BOT_TOKEN:
-    raise ValueError("Токен не найден! Создайте файл .env с BOT_TOKEN=ваш_токен")
+ADMIN_ID = 7878666092  # Ваш ID
